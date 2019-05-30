6a5ce6c9e32783c5ce674e5856c1a31c06038577
sowmyashreeks <sowmya.ks.65@gmail.com>
2019-05-31 00:24:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'importExcell';
+  willDownload = false;
+  output:any;
+data:any;
+  dataString: any;
+  constructor() { }
+
+  onFileChange(ev) {
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {});
+      this.data = Object.keys(jsonData).map(key=>jsonData[key])
+      console.log(this.data);
+      this.data.map(elm => {
+        this.dataString = elm;
+        console.log('===>',this.dataString);
+      });
+      // this.output = dataString.slice(0, 300);
+      // this.setDownload(dataString);
+    }
+    reader.readAsBinaryString(file);
+  }
+
+
+  setDownload(data) {
+    this.willDownload = true;
+    setTimeout(() => {
+      const el = document.querySelector("#download");
+      el.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
+      el.setAttribute("download", 'xlsxtojson.json');
+    }, 1000)
+  }
+
+
+
 }
